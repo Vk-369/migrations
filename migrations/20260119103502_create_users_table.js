0687bbd73d769fe3f175c6f4a9406b40e00e3cd9
vk369 <gavaravinay1@gmail.com>
2026-01-19 16:38:00 +0530

    changes

--- migrations/20260119103502_create_users_table.js
+++ migrations/20260119103502_create_users_table.js
@@ -2,7 +2,7 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {  
+exports.up = function(knex) {    
   return knex.schema.createTable('users', (table) => {
     table.increments('id').primary();
     table.string('email').unique().notNullable();
